96f4c5b4f3fecd463fa02206633b8c21ee64d0ab
tfranx <tfranks.fsae@gmail.com>
2017-04-26 19:51:58 -0500

    Update Gauss_Seidel.m
    Added in P matrix and finished applying all boundary conditions.

--- Gauss_Seidel.m
+++ Gauss_Seidel.m
@@ -22,20 +22,50 @@ A = C - (2 * (DX^2)) - (2 * (DY^2));
 B = (DX^2) * (DY^2);
 %Determining the sizes, Element_Count, of the matrices to be evaluated:
 Element_Count = (N+2) * M; %+2 comes from the two Neumann boundary conditions imposing ghost nodes
-%Generating original K (left hand side coefficients) and P (right hand side
-%known values) matrices:
+%Solving for F(X,Y) values, "F", arrayed as a matrix:
+F = zeros(Element_Count, 1);
+for j = 1:M
+    for i = 1:(N+2)
+        F(i + (N+2) * (j-1), 1) = cos((pi() / 2) * ((((i - 1) * DX) / pi()) + 1)) * sin((j * DY) / 2);
+    end
+end
+% Determining values of U determined from the Dirichlet Boundary Conditions
+% imposed onto the Y-domain:
+Dirichlet_Val_Lower = zeros(N+2, 1);
+Dirichlet_Val_Upper = zeros(N+2, 1);
+for i = 1:(N+2)
+    Dirichlet_Val_Lower(i, 1) = cos(pi() * DX * (i-1)) * cosh(2 * pi() - (DX * (i-1))); %Applies to known U values determined from the Dirichlet Boundary Condition imposed at Y = -pi
+    Dirichlet_Val_Upper(i, 1) = (((i-1) * DX)^2) * sin(((i-1) * DX) / 4); %Applies to known U values determined from the Dirichlet Boundary Condition imposed at Y = pi
+end
+%Generating original K (left hand side coefficients) matrix:
 K = zeros(Element_Count, Element_Count);
-P = zeros(Element_Count, 1);
 for j = 1:M %i + (N+2) * (j-1) for indexing the column values
     for i = 1:(N+2)
         K(i + (N+2) * (j-1), i + (N+2) * (j-1)) = A; %Fills the diagonal with the A values
         if (i == 1)
-            K(i + (N+2) * (j-1), i + (N+2) * (j-1) + 1) = 2 * (DY^2);
+            K(i + (N+2) * (j-1), i + (N+2) * (j-1) + 1) = 2 * (DY^2); %Fills all points in matrix where the Neumann Boundary Condition for the left end of the x domain produces 2*DY^2
         elseif (i == N+2)
-            K(i + (N+2) * (j-1), i + (N+2) * (j-1) - 1) = 2 * (DY^2);
+            K(i + (N+2) * (j-1), i + (N+2) * (j-1) - 1) = 2 * (DY^2); %Fills all points in matrix where the Neumann Boundary Condition for the right end of the x domain produces 2*DY^2
         else
-            K(i + (N+2) * (j-1), i + (N+2) * (j-1) + 1) = DY^2;
-            K(i + (N+2) * (j-1), i + (N+2) * (j-1) - 1) = DY^2;
+            K(i + (N+2) * (j-1), i + (N+2) * (j-1) + 1) = DY^2; %Fills the upper portion of the center tridiagonal with DY^2, where applicable
+            K(i + (N+2) * (j-1), i + (N+2) * (j-1) - 1) = DY^2; %Fills the lower portion of the center tridiagonal with DY^2, where applicable
         end
+        if (j > 1)
+            K(i + (N+2) * (j-2), i + (N+2) * (j-1)) = DX^2; %Fills the uppermost diagonal with values of DX^2
+        end
+        if (j < M)
+            K(i + (N+2) * (j), i + (N+2) * (j-1)) = DX^2; %Fills the lowest diagonal with values of DX^2
+        end
+    end
+end
+%Generating original P (right hand side known values) matrix:
+P = zeros(Element_Count, 1);
+for i = 1:Element_Count
+    if (i <= (N+2))
+        P(i,1) = B * F(i, 1) - ((DX^2) * Dirichlet_Val_Lower(i, 1)); %All values impacted by lower Y domain (at Y = -pi) Dirichlet Boundary Condition
+    elseif (i > (Element_Count - (N+2)))
+        P(i,1) = B * F(i, 1) - ((DX^2) * Dirichlet_Val_Upper(i - ((N+2) * (M-1)), 1));  %All values impacted by upper Y domain (at Y = pi) Dirichlet Boundary Condition
+    else
+        P(i,1) = B * F(i, 1); %All values not impacted by Dirichlet Boundary Conditions
     end
 end
