1307c45f8c741b08cdd98a4f0adc0bf8214a8fa4
tfranx <tfranks.fsae@gmail.com>
2017-04-30 18:12:21 -0500

    Included SOR iterations to code
    Inserted input for lambda (G) and added iterations for SOR to approximation while loop.

--- SOR_Final_Project.m
+++ SOR_Final_Project.m
@@ -8,6 +8,8 @@ X_Internal_Nodes = input('Enter value of X_Internal_Nodes, the number of interna
 Y_Internal_Nodes = input('Enter value of Y_Internal_Nodes, the number of internal nodes for the Y-domain: ');
 %Setting the value of C (capital lambda in problem statement):
 C = input('Enter value of C, the given constant for capital lambda: ');
+%Setting the value of G (Overrelaxation variable):
+G = input('Enter value of G, the lambda coefficient, to be between 1 and 2, to use for SOR (overrelaxation)');
 %Setting the value of Es, the acceptable limit of error for system convergence:
 Es = 10^-10;
 %Defining L, the length of the X and Y domains:
@@ -63,10 +65,12 @@ if (A ~= 0)%Set condition for just in case the variable coefficient is equal to 
         for j = 2:MM
             W(1,j) = U(1,j); %W saves value of U for error calculation
             U(1,j) = (- 2 * (DY^2) * U(2,j) - (DX^2) * U(1,j-1) - (DX^2) * U(1,j+1)) / A;  %cos((pi() / 2) * (0 + 1)) = 0 so Fi,j = 0, Neumann condition for X = -pi
+            U(1,j) = (G * U(1,j)) + ((1-G) * W(1,j)); %Applying SOR iteration
             Error(1,j) = abs((U(1,j) - W(1,j)) / U(1,j)); %Computes relative error for this calculation inside this iteration
             
             W(N,j) = U(N,j); %W saves value of U for error calculation
             U(N,j) = (- 2 * (DY^2) * U(NN,j) - (DX^2) * U(N,j-1) - (DX^2) * U(N,j+1)) / A; %cos(3 * pi() / 2) = 0 so Fi,j = 0, Neumann condition for X = pi
+            U(N,j) = (G * U(N,j)) + ((1-G) * W(N,j)); %Applying SOR iteration
             Error(N,j) = abs((U(N,j) - W(N,j)) / U(N,j)); %Computes relative error for this calculation inside this iteration
         end
         %Evaluating for general expression (internal nodes):
@@ -74,6 +78,7 @@ if (A ~= 0)%Set condition for just in case the variable coefficient is equal to 
             for i = 2:NN
                 W(i,j) = U(i,j); %W saves value of U for error calculation
                 U(i,j) = (B * (cos((pi() / 2) * ((((i-1) * DX) / pi()) + 1)) * sin(((j - 1) * DY) / 2)) - (DY^2) * U(i-1,j) - (DY^2) * U(i+1,j) - (DX^2) * U(i,j-1) - (DX^2) * U(i,j+1)) / A;
+                U(i,j) = (G * U(i,j)) + ((1-G) * W(i,j)); %Applying SOR iteration
                 Error(i,j) = abs((U(i,j) - W(i,j)) / U(i,j)); %Computes relative error for this calculation inside this iteration
             end
         end
